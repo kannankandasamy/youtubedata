eca8722b95a6fe482ec7dd72bb525b4d981c6a60
kannan Kandasamy <kannanvijay@hotmail.com>
2023-11-10 11:45:56 -0500

    changed few answers to charts

--- data_viewer.py
+++ data_viewer.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 from streamlit_option_menu import option_menu
+import matplotlib.pyplot as plt
 
 from library.youtube_analysis import *
 from library.mongod_load import *
@@ -79,84 +80,119 @@ class DataViewer:
             pl_df = mys.get_data_from_mysql(query.format(channel_selected))
             #print(chn_df)                
             st.dataframe(pl_df
-                        ,hide_index=True,width=1200)      
-
-            st.write("2. Most Number videos")    
-            query = """select channel_name, count(*) video_count from videos
-                        group by channel_name
-                        order by video_count desc limit 10;
-                        """
-            df2 = mys.get_data_from_mysql(query)
-            #print(chn_df)                
-            st.dataframe(df2
-                        ,hide_index=True,width=1200)                                    
-        
-            st.write("3. Most viewed videos")    
-            query = """select video_title as vidoe_name, channel_name, video_view_count from videos
-                        order by video_view_count desc
-                        limit 10;"""
-            df3 = mys.get_data_from_mysql(query)
-            #print(chn_df)                
-            st.dataframe(df3
-                        ,hide_index=True,width=1200)       
-
-            st.write("4. Most viewed videos")    
-            query = """select v.video_title as video_name, count(*) as comments_count from videos v
-                        join comments c
-                        on v.video_id = c.video_id
-                        group by v.video_title
-                        order by v.video_title;"""
-            df4 = mys.get_data_from_mysql(query)
-            #print(chn_df)                
-            st.dataframe(df4
-                        ,hide_index=True,width=1200)   
-
-            st.write("5. Highest Number of likes")    
-            query = """select video_title as vidoe_name, channel_name, video_like_count from videos
-                        order by video_like_count desc
-                        limit 10;"""
-            df5 = mys.get_data_from_mysql(query)
-            #print(chn_df)                
-            st.dataframe(df5
-                        ,hide_index=True,width=1200)       
-
-
-            st.write("7. Total Number of views for a channel")    
-            query = """select channel_name, view_count from channels
-                        order by view_count desc limit 10;"""
-            df7 = mys.get_data_from_mysql(query)
-            #print(chn_df)                
-            st.dataframe(df7
-                        ,hide_index=True,width=1200)      
-
-
-            st.write("8. Videos published on 2022 with channel names")    
-            query = """select channel_name, count(*) as videos_on_2022 from videos where year(date(video_publishet_at))=2022
-                        group by channel_name
-                        having count(*)>0;"""
-            df8 = mys.get_data_from_mysql(query)
-            #print(chn_df)                
-            st.dataframe(df8
                         ,hide_index=True,width=1200)     
 
+            #st.write("Select question")
+            query = """select question_id, question_name from questions order by question_id ;"""
+            qn_df1 = mys.get_data_from_mysql(query)
+            question_selected = st.selectbox("Select Question", options = qn_df1['question_name'])            
 
-            st.write("9. Average duration of videos in channels")    
-            query = """select channel_name, round(avg(TIME_TO_SEC(STR_TO_DATE(video_duration, 'PT%iM%sS')))) as average_duration_seconds from videos
-                        group by channel_name;"""
-            df9 = mys.get_data_from_mysql(query)
-            #print(chn_df)                
-            st.dataframe(df9
-                        ,hide_index=True,width=1200)  
+            if question_selected.startswith("1."):
+                st.write("1. Videos list for selected channel")    
+                query = """select video_title as Video_Name from videos where channel_name = '{}';"""
+                pl_df = mys.get_data_from_mysql(query.format(channel_selected))
+                #print(chn_df)                
+                st.dataframe(pl_df
+                            ,hide_index=True,width=1200)                   
+            elif question_selected.startswith("2."):                
+                st.write("2. Most Number videos")    
+                query = """select channel_name, count(*) video_count from videos
+                            group by channel_name
+                            order by video_count desc limit 10;
+                            """
+                df2 = mys.get_data_from_mysql(query)
+                #print(chn_df)                
+                st.dataframe(df2
+                            ,hide_index=True,width=1200)     
 
+                st.write("In chart")
+                fig1, ax1 = plt.subplots()
+                ax1.pie(df2["video_count"], labels=df2["channel_name"])
+                ax1.axis("equal")
+                st.pyplot(fig1)
 
-            st.write("10. Highest number of comments for a channel")    
-            query = """select v.channel_name as channel_name, count(*) as comments_count from videos v
-                        join comments c
-                        on v.video_id = c.video_id
-                        group by v.channel_name
-                        order by comments_count desc
-                        limit 10;         """
-            df10 = mys.get_data_from_mysql(query)
-            #print(chn_df)                
-            st.dataframe(df10
-                        ,hide_index=True,width=1200)   
+            elif question_selected.startswith("3."):
+                st.write("3. Most viewed videos")    
+                query = """select video_title as video_name, channel_name, video_view_count from videos
+                            order by video_view_count desc
+                            limit 10;"""
+                df3 = mys.get_data_from_mysql(query)
+                #print(chn_df)                
+                st.dataframe(df3
+                            ,hide_index=True,width=1200)      
+                 
+                st.write("In bar chart")
+                st.bar_chart(df3, x="video_name",y="video_view_count",color="video_name")
+
+            elif question_selected.startswith("4."):
+                st.write("4. Most viewed videos")    
+                query = """select v.video_title as video_name, count(*) as comments_count from videos v
+                            join comments c
+                            on v.video_id = c.video_id
+                            group by v.video_title
+                            order by v.video_title;"""
+                df4 = mys.get_data_from_mysql(query)
+                #print(chn_df)                
+                st.dataframe(df4
+                            ,hide_index=True,width=1200)   
+            elif question_selected.startswith("5."):
+                st.write("5. Highest Number of likes")    
+                query = """select video_title as vidoe_name, channel_name, video_like_count from videos
+                            order by video_like_count desc
+                            limit 10;"""
+                df5 = mys.get_data_from_mysql(query)
+                #print(chn_df)                
+                st.dataframe(df5
+                            ,hide_index=True,width=1200)       
+            elif question_selected.startswith("6."):
+                st.write("Youtube returns only likes count not the dislike counts")
+            elif question_selected.startswith("7."):
+                st.write("7. Total Number of views for a channel")    
+                query = """select channel_name, view_count from channels
+                            order by view_count desc limit 10;"""
+                df7 = mys.get_data_from_mysql(query)
+                #print(chn_df)                
+                st.dataframe(df7
+                            ,hide_index=True,width=1200)  
+
+                st.write("In chart")
+                fig1, ax1 = plt.subplots()
+                ax1.pie(df7["view_count"], labels=df7["channel_name"])
+                ax1.axis("equal")
+                st.pyplot(fig1)
+
+            elif question_selected.startswith("8."):
+                st.write("8. Videos published on 2022 with channel names")    
+                query = """select channel_name, count(*) as videos_on_2022 from videos where year(date(video_publishet_at))=2022
+                            group by channel_name
+                            having count(*)>0;"""
+                df8 = mys.get_data_from_mysql(query)
+                #print(chn_df)                
+                st.dataframe(df8
+                            ,hide_index=True,width=1200)   
+
+                st.write("In bar chart")
+                st.bar_chart(df8, x="channel_name",y="videos_on_2022",color="channel_name")
+
+            elif question_selected.startswith("9."):
+                st.write("9. Average duration of videos in channels")    
+                query = """select channel_name, round(avg(TIME_TO_SEC(STR_TO_DATE(video_duration, 'PT%iM%sS')))) as average_duration_seconds from videos
+                            group by channel_name;"""
+                df9 = mys.get_data_from_mysql(query)
+                #print(chn_df)                
+                st.dataframe(df9
+                            ,hide_index=True,width=1200)  
+            elif question_selected.startswith("10."):
+                st.write("10. Highest number of comments for a channel")    
+                query = """select v.channel_name as channel_name, count(*) as comments_count from videos v
+                            join comments c
+                            on v.video_id = c.video_id
+                            group by v.channel_name
+                            order by comments_count desc
+                            limit 10;         """
+                df10 = mys.get_data_from_mysql(query)
+                #print(chn_df)                
+                st.dataframe(df10
+                            ,hide_index=True,width=1200)   
+            else:
+                st.write("Select valid option")
--- ytube.py
+++ ytube.py
@@ -1,31 +1,106 @@
 import pandas as pd
 import streamlit as st
 import googleapiclient.discovery
+import configparser
+
+def get_data_config():
+    conf = {}
+    cfg = configparser.ConfigParser()
+    cfg.read('data.conf')
+    for (key,val) in cfg.items('DATA_APP_CONFIGS'):
+        conf[key]=val
+    return conf
+
+def get_api_connection(api_key):
+    api_service_name = "youtube"
+    api_version = "v3"
+    youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=api_key)
+    return youtube
 
 
-class Youtube:
-    def __init__(self, api_key):
-        self.api_key = api_key
 
-	def get_api_connection(self):
-		api_service_name = "youtube"
-		api_version = "v3"
-		youtube = googleapiclient.discovery.build(api_service_name, api_version, developerKey=api_key)
-		return youtube     											   
+#Adding comments to include nextPageToken
 
-class Mongod:
-    def __init__(self, connection):
-        self.conn = connection
+def get_comments_full_details(youtube,vid_id_details):
+    comment_details = []
 
-class MySql:
-    def __init__(self, connection):
-        self.conn = connection
+    for vid in vid_id_details:
+        next_page_token = None
+        print(vid[0])
+        print(len(comment_details))
+        while True:
+            try:
+                request4=youtube.commentThreads().list(
+                    part="snippet",
+                    videoId=vid[0],
+                    maxResults=100,
+                    order="time",
+                    textFormat="plainText",
+                    pageToken=next_page_token
+                )
+                response4 = request4.execute()
 
-class DataAnalytics:
-    if __name__=="__main__":
+                for item in response4["items"]:
+                    data = dict(comment_id=item['snippet']['topLevelComment']['id'],
+                                video_id=item['snippet']['topLevelComment']['snippet']['videoId'],
+                                comment_text=item['snippet']['topLevelComment']['snippet']['textDisplay'],
+                                comment_author=item['snippet']['topLevelComment']['snippet']['authorDisplayName'],
+                                comment_published_at=item['snippet']['topLevelComment']['snippet']['publishedAt']
+                                )
+                    comment_details.append(data)
+                if next_page_token is None:
+                    break
+            except:
+                pass
+    return comment_details
 
-        api_key = "***"
-        yd = Youtube(api_key)
-        print(yd.api_key)
-		
-        youtube = yd.get_api_connection()       
+def get_video_headers(youtube, channel_id):
+    #channel_id = "UCDjEYBdbvu-1DpWukvNu8vA"
+    vid_ids = []
+    request = youtube.channels().list(
+        part="contentDetails",
+        id=channel_id
+    )
+    response = request.execute()
+    playlist_id=response['items'][0]['contentDetails']['relatedPlaylists']['uploads']
+
+    next_page_token = None
+
+    while True:
+        request1 = youtube.playlistItems().list(
+            part='snippet',
+            playlistId=playlist_id,
+            maxResults=50,
+            pageToken=next_page_token
+        )
+        response1 = request1.execute()
+        for i in range(len(response1['items'])):
+            vid_ids.append((response1['items'][i]['snippet']['resourceId']['videoId'], response1['items'][i]['snippet']['publishedAt']))
+        next_page_token=response1.get('nextPageToken')
+        if next_page_token is None:
+            break
+    return vid_ids
+
+api_key = get_data_config()
+youtube = get_api_connection(api_key)
+
+comment_details = []
+request4=youtube.commentThreads().list(
+    part="snippet",
+    videoId="VO1TybI4pGc",
+    maxResults=25,
+    order="time",
+    textFormat="plainText"
+)
+response4 = request4.execute()
+
+for item in response4["items"]:
+    data = dict(comment_id=item['snippet']['topLevelComment']['id'],
+                video_id=item['snippet']['topLevelComment']['snippet']['videoId'],
+                comment_text=item['snippet']['topLevelComment']['snippet']['textDisplay'],
+                comment_author=item['snippet']['topLevelComment']['snippet']['authorDisplayName'],
+                comment_published_at=item['snippet']['topLevelComment']['snippet']['publishedAt']
+                )
+    comment_details.append(data)
+
+vid_id_details = get_video_headers(youtube, "UCDA9RcW2ftEWIh5wS6dkhJA")
